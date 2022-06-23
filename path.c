4e2f8aadde1ba6de7df47ab0fcc287f96382807f
Deepan K S <76985697+Deeps-01@users.noreply.github.com>
2022-06-23 14:04:02 +0530

    Update path.c

--- path.c
+++ path.c
@@ -1,207 +1,143 @@
-/*
- * This is a openmp version of bellman_ford algorithm
- * Compile: g++ -std=c++11 -o openmp_bellman_ford openmp_bellman_ford.cpp
- * Run: ./openmp_bellman_ford <input file> <number of threads>, you will find the output file 'output.txt'
- * */
+//single src shortest path
 
-#include <string>
-#include <cassert>
-#include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <iomanip>
-#include <cstring>
-#include <sys/time.h>
-
-#include "omp.h"
-
-using std::string;
-using std::cout;
-using std::endl;
-
-#define INF 1000000
-
-/**
- * utils is a namespace for utility functions
- * including I/O (read input file and print results) and matrix dimension convert(2D->1D) function
- */
-namespace utils {
-    int N; //number of vertices
-    int *mat; // the adjacency matrix
-
-    void abort_with_error_message(string msg) {
-        std::cerr << msg << endl;
-        abort();
-    }
-
-    //translate 2-dimension coordinate to 1-dimension
-    int convert_dimension_2D_1D(int x, int y, int n) {
-        return x * n + y;
-    }
-
-    int read_file(string filename) {
-        std::ifstream inputf(filename, std::ifstream::in);
-        if (!inputf.good()) {
-            abort_with_error_message("ERROR OCCURRED WHILE READING INPUT FILE");
-        }
-        inputf >> N;
-        //input matrix should be smaller than 20MB * 20MB (400MB, we don't have too much memory for multi-processors)
-        assert(N < (1024 * 1024 * 20));
-        mat = (int *) malloc(N * N * sizeof(int));
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++) {
-                inputf >> mat[convert_dimension_2D_1D(i, j, N)];
-            }
-        return 0;
-    }
-
-    int print_result(bool has_negative_cycle, int *dist) {
-        std::ofstream outputf("output.txt", std::ofstream::out);
-        if (!has_negative_cycle) {
-            for (int i = 0; i < N; i++) {
-                if (dist[i] > INF)
-                    dist[i] = INF;
-                outputf << dist[i] << '\n';
-            }
-            outputf.flush();
-        } else {
-            outputf << "FOUND NEGATIVE CYCLE!" << endl;
-        }
-        outputf.close();
-        return 0;
-    }
-}//namespace utils
+#include <stdio.h>
+#include <string.h>
+#include<limits.h>
+#include <stdlib.h>
+#include <omp.h>
+#include<stdbool.h>
 
 
-/**
- * Bellman-Ford algorithm. Find the shortest path from vertex 0 to other vertices.
- * @param p number of processes
- * @param n input size
- * @param *mat input adjacency matrix
- * @param *dist distance array
- * @param *has_negative_cycle a bool variable to recode if there are negative cycles
-*/
-void bellman_ford(int p, int n, int *mat, int *dist, bool *has_negative_cycle) {
 
-    int local_start[p], local_end[p];
-    *has_negative_cycle = false;
+struct Edge {
+	int src, dest, weight;
+};
 
-    //step 1: set openmp thread number
-    omp_set_num_threads(p);
+struct Graph {
+	
+	int V, E;
+	struct Edge* edge;
+};
 
-    //step 2: find local task range
-    int ave = n / p;
-#pragma omp parallel for
-    for (int i = 0; i < p; i++) {
-        local_start[i] = ave * i;
-        local_end[i] = ave * (i + 1);
-        if (i == p - 1) {
-            local_end[i] = n;
-        }
-    }
 
-    //step 3: bellman-ford algorithm
-    //initialize distances
-#pragma omp parallel for
-    for (int i = 0; i < n; i++) {
-        dist[i] = INF;
-    }
-    //root vertex always has distance 0
-    dist[0] = 0;
-
-    int iter_num = 0;
-    bool has_change;
-    bool local_has_change[p];
-#pragma omp parallel
-    {
-        int my_rank = omp_get_thread_num();
-        //bellman-ford algorithm
-        for (int iter = 0; iter < n - 1; iter++) {
-            local_has_change[my_rank] = false;
-            for (int u = 0; u < n; u++) {
-                for (int v = local_start[my_rank]; v < local_end[my_rank]; v++) {
-                    int weight = mat[utils::convert_dimension_2D_1D(u, v, n)];
-                    if (weight < INF) {
-                        int new_dis = dist[u] + weight;
-                        if (new_dis < dist[v]) {
-                            local_has_change[my_rank] = true;
-                            dist[v] = new_dis;
-                        }
-                    }
-                }
-            }
-#pragma omp barrier
-#pragma omp single
-            {
-                iter_num++;
-                has_change = false;
-                for (int rank = 0; rank < p; rank++) {
-                    has_change |= local_has_change[rank];
-                }
-            }
-            if (!has_change) {
-                break;
-            }
-        }
-    }
-
-    //do one more iteration to check negative cycles
-    if (iter_num == n - 1) {
-        has_change = false;
-        for (int u = 0; u < n; u++) {
-#pragma omp parallel for reduction(|:has_change)
-            for (int v = 0; v < n; v++) {
-                int weight = mat[u * n + v];
-                if (weight < INF) {
-                    if (dist[u] + weight < dist[v]) { // if we can relax one more step, then we find a negative cycle
-                        has_change = true;;
-                    }
-                }
-            }
-        }
-        *has_negative_cycle = has_change;
-    }
-
-    //step 4: free memory (if any)
+struct Graph* createGraph(int V, int E)
+{
+	struct Graph *graph = (struct Graph *)malloc(sizeof(struct Graph));
+	graph->V = V;
+	graph->E = E;
+	graph->edge = (struct Edge *)malloc(graph->E * sizeof(struct Edge));
+	return graph;
 }
 
-int main(int argc, char **argv) {
-    if (argc <= 1) {
-        utils::abort_with_error_message("INPUT FILE WAS NOT FOUND!");
+
+void printans(int dist[], int n,double t1,double t2)
+{
+	printf("Vertex Distance from Source\n");
+	for (int i = 0; i < n; ++i)
+		printf("%d \t\t %d\n", i, dist[i]);
+    
+    printf("total time taken is %g",t2-t1);    
+	
+}
+
+ 
+void BellmanFord(int graph[][3] ,int V,int E, int src)
+{
+	
+int dist[V];
+bool change = true;
+omp_set_num_threads(8);
+
+double t1=omp_get_wtime();
+
+   #pragma omp parallel for
+	for (int i = 0; i < V; i++)
+		dist[i] = INT_MAX;
+
+	dist[src] = 0;
+
+
+	while(change){    
+        change=false;
+		#pragma omp parallel for default(none) shared(E,dist,graph,change)
+		for (int j = 0; j < E; j++){
+			int u = graph[j][0];
+			int v = graph[j][1];
+			int wt = graph[j][2];
+
+				if (dist[u] != INT_MAX && dist[u] + wt < dist[v]){
+					dist[v] = dist[u] + wt;
+					change=true;
+				}		
+		}
+	}
+
+
+        #pragma omp single 
+		{
+        for(int j=0;j<E;j++)
+			{
+				int u = graph[j][0];
+		        int v = graph[j][1];
+		        int wt = graph[j][2];
+		        if (dist[u] != INT_MAX && dist[u] + wt < dist[v])
+				{
+					printf("negative cycle is pres\n");
+				}
+			}
+		}	   
+				
+double t2=omp_get_wtime();
+
+    #pragma omp single
+	printans(dist, V,t1,t2);
+}
+
+
+
+int main(int argc,char *argv[])
+{ 
+    int V = 5; // Number of vertices in graph
+    int E = 10; // Number of edges in graph
+    int graph[E][3];
+
+     FILE* ptr;
+
+ 
+    // Opening file in reading mode
+    ptr = fopen("data", "r");
+ 
+    if (NULL == ptr) {
+        printf("file can't be opened \n");
     }
-    if (argc <= 2) {
-        utils::abort_with_error_message("NUMBER OF THREADS WAS NOT FOUND!");
-    }
-    string filename = argv[1];
-    int p = atoi(argv[2]);
+ 
+    printf("content of this file are \n");
 
-    int *dist;
-    bool has_negative_cycle = false;
+    char ch[50];
 
+    int i=0,j=0;
 
-    assert(utils::read_file(filename) == 0);
-    dist = (int *) malloc(sizeof(int) * utils::N);
+   while( fgets (ch,10,ptr)!=NULL ) {
 
-    //time counter
-    timeval start_wall_time_t, end_wall_time_t;
-    float ms_wall;
+        char * token = strtok(ch, " ");
+        while( token != NULL ) {
+            if(j==3)
+            {
+            j=0;
+            i++;
+            }
+          int t=atoi(token);
+            graph[i][j]=t;
+            j++;
 
-    //start timer
-    gettimeofday(&start_wall_time_t, nullptr);
+         token = strtok(NULL, " ");
+   }       }
 
-    //bellman-ford algorithm
-    bellman_ford(p, utils::N, utils::mat, dist, &has_negative_cycle);
-
-    //end timer
-    gettimeofday(&end_wall_time_t, nullptr);
-    ms_wall = ((end_wall_time_t.tv_sec - start_wall_time_t.tv_sec) * 1000 * 1000
-               + end_wall_time_t.tv_usec - start_wall_time_t.tv_usec) / 1000.0;
-
-    std::cerr.setf(std::ios::fixed);
-    std::cerr << std::setprecision(6) << "Time(s): " << (ms_wall / 1000.0) << endl;
-    utils::print_result(has_negative_cycle, dist);
-    free(dist);
-    free(utils::mat);
+ 
+    // Closing the file
+    fclose(ptr);
+	
+    BellmanFord(graph, V, E, 0);
 
     return 0;
 }
