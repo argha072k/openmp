1f774912279287ae98fa61b6b97c14844596ecad
Deepan K S <76985697+Deeps-01@users.noreply.github.com>
2022-06-24 15:40:53 +0530

    Update path.cpp

--- path.cpp
+++ path.cpp
@@ -15,11 +15,11 @@
 
 using namespace std;
 
-/*
+
 struct Edge {
 	int src, dest, weight;
 };
-
+/*
 struct Graph {
 	
 	int V, E;
@@ -37,29 +37,40 @@ struct Graph* createGraph(int V, int E)
 }
 */
 
-void printans(int dist[], int n,double t1,double t2)
+void printans(vector<long> dist, int n,double t1,double t2)
 {
+	//storing in a file
+
+	ofstream MyFile("parallel_output.txt");
+	MyFile<<"Vertex Distance from Source\n";
+	MyFile<<"NodeId\tShortest distance from source \n";
+	for (int i = 0; i < n; ++i)
+		MyFile<<i<<" \t\t "<<dist[i]<<"\n";
+
+  // Close the file
+  MyFile.close();
+
+  //displaying in terminal
 	printf("Vertex Distance from Source\n");
 	for (int i = 0; i < n; ++i)
-		printf("%d \t\t %d\n", i, dist[i]);
+		printf("%d \t\t %ld\n", i, dist[i]);
     
     printf("total time taken is %g",t2-t1);    
 	
 }
 
  
-void BellmanFord(int graph[][3] ,int V,int E, int src)
+void BellmanFord(vector <Edge> graph ,int V,int E, int src)
 {
-	
-int dist[V];
+vector<long> dist(V);	
+
 bool change = true;
-omp_set_num_threads(8);
+omp_set_num_threads(16);
 
 double t1=omp_get_wtime();
-
    #pragma omp parallel for
 	for (int i = 0; i < V; i++)
-		dist[i] = INT_MAX;
+		dist[i]=INT_MAX;
 
 	dist[src] = 0;
 
@@ -68,9 +79,9 @@ double t1=omp_get_wtime();
         change=false;
 		#pragma omp parallel for default(none) shared(E,dist,graph,change)
 		for (int j = 0; j < E; j++){
-			int u = graph[j][0];
-			int v = graph[j][1];
-			int wt = graph[j][2];
+			int u = graph[j].src;
+			int v = graph[j].dest;
+			int wt = graph[j].weight;
 
 				if (dist[u] != INT_MAX && dist[u] + wt < dist[v]){
 					dist[v] = dist[u] + wt;
@@ -84,9 +95,9 @@ double t1=omp_get_wtime();
 		{
         for(int j=0;j<E;j++)
 			{
-				int u = graph[j][0];
-		        int v = graph[j][1];
-		        int wt = graph[j][2];
+				int u = graph[j].src;
+		        int v = graph[j].dest;
+		        int wt = graph[j].weight;
 		        if (dist[u] != INT_MAX && dist[u] + wt < dist[v])
 				{
 					printf("negative cycle is pres\n");
@@ -106,7 +117,7 @@ int main(int argc,char *argv[])
 { 
     int V = 1971259; // Number of vertices in graph
     int E = 5533214; // Number of edges in graph
-    int graph[E][3];
+    vector <Edge>g;
 
 	ifstream infile;
 	infile.open("roadNet-CA.txt");
@@ -120,16 +131,16 @@ int main(int argc,char *argv[])
 		}
 
 		stringstream ss(line);
-
-		ss>>graph[i][0];
-		ss>>graph[i][1];
-		graph[i][2]=3;
+		Edge inp;
+		ss>>inp.src;
+		ss>>inp.dest;
+		inp.weight=2;
 		i++;
 		
+		g.push_back(inp);
 	}
 
-
-    BellmanFord(graph, V, E, 0);
+    BellmanFord(g, V, E, 0);
 
     return 0;
 }
